9cdde3ac6a589b6541e315d8d749cbde9c78e34c
皓博 <275497339@qq.com>
2025-02-20 16:24:59 +0800

    Add files via upload

--- haobo_tel_api_post.py
+++ haobo_tel_api_post.py
@@ -2,6 +2,11 @@ import logging
 import re
 import requests
 import json
+import pytesseract
+try:
+    from PIL import Image
+except ImportError:
+    import Image
 from telegram import Update
 from telegram.ext import filters,ApplicationBuilder, ContextTypes, CommandHandler,MessageHandler
 
@@ -12,24 +17,75 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello,{update.effective_user.username}, , Welcome to CHECK PCID!  \n 👤 ID: <tg-spoiler>{update.effective_user.id}</tg-spoiler>",parse_mode='HTML')
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def transfer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text:
        iid = "".join(re.findall("\\d+", update.message.text))
-       if len(iid) == 63 or len(iid) == 54:
+       d2 = "".join(re.findall("\\w+", update.message.text))
+       if len(d2) == 25:
+           await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f'Received {update.effective_user.first_name}request.key:{d2}')
+           url1 = "https://api.f753.com/api_pid"
+
+           data = {'key': d2,
+                   'tid': 'you_id',
+                   'api': 'you_api'
+                   }
+           key = requests.post(url1, data=data)
+           reply_key = "KEY:{}\nVersion:{}\nKEYstate:{}\nonline:{}\nPrompt code2:{}\nTIME:{}".format(
+               key.json()['data_key']['key'], key.json()['data_key']['Version'], key.json()['data_key']['KEYstate'],
+               key.json()['data_key']['online'], key.json()['data_key']['Prompt code2:'], key.json()['data_key']['TIME'])
+           await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{reply_key}')
+       elif len(iid) == 63 or len(iid) == 54:
            await context.bot.send_message(chat_id=update.effective_chat.id,text=f'receive {update.effective_user.first_name} Request iid:{iid}')
-           url1 = "https://api.f753.com/api_get"
+           url1 = "https://api.haobo.org/api_get"
            data = {'id': iid,
-                   'tid': 'youid',
-                   'tuser': 'None',
+                   'tid': 'you_id',
+                   'tuser': 'user',
                    'api': 'you_api'
                    }
            cid = requests.post(url1, data=data)
-           await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cid.json()}')
+           cids = f"IID:{cid.json()['cid_data']['IID:']}\nCID:{cid.json()['cid_data']['CID:']}"
+           await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cids}')
        else:
-           cid_message = "The number of iIDs is incorrect, please check."
-           await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cid_message}')
+           pass
+           #cid_message = "The number of iIDs is incorrect, please check."
+           #await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cid_message}')
+    elif update.message.photo:
+        file_id = update.message.photo[-1].file_id
+        print(file_id)
+        the_file = await context.bot.get_file(file_id)
+        print(the_file)
+        response = requests.get(the_file.file_path)
+        if response.status_code == 200:
+           with open('local_image2.jpg', 'wb') as f:
+               # 写入获取到的内容
+               f.write(response.content)
+        else:
+            pass
+        xx = re.findall("\\d+", pytesseract.image_to_string(Image.open('local_image2.jpg',)))
+        list_Img = []
+        for x in xx:
+            if len(x) == 6 or len(x) == 7:
+                list_Img.append(x)
+
+        x1 = "".join(list_Img)
+        if len(x1) == 63 or len(x1) == 54:
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text=f'Received {update.effective_user.first_name}Request:{x1}')
+
+            url1 = "https://api.haobo.org/api_get"
+            data = {'id': x1,
+                    'tid': 'you_id',
+                    'tuser': 'user',
+                    'api': 'you_id'
+                    }
+            cid = requests.post(url1, data=data)
+            cids = f"IID:{cid.json()['cid_data']['IID:']}\nCID:{cid.json()['cid_data']['CID:']}"
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cids}')
+        else:
+            pass
 
 
 
@@ -37,10 +93,9 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('your token').build()
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    transfer_handler = MessageHandler((~filters.COMMAND), transfer)
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-
+    application.add_handler(transfer_handler)
     application.run_polling()
     application.run_polling(allowed_updates=Update.ALL_TYPES)
